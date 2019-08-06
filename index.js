3cc2e830edd0a5ce45deeff0533ff60b749ecbe8
侯建东 <houjiandong@houjiandongdeMacBook-Pro.local>
2019-08-06 12:05:04 +0800

    debugger

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import * as prototypes from './prototype/index'
 function setPrototype(main, obj) {
   Object.keys(obj).forEach((item) => {
     let key = '$' + item
-    main.prototype.key = obj[item]
+    main.prototype[key] = obj[item]
   })
 }
 export default function install(Vue) {
